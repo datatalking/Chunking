018a9e38ba93dd3c71146cc79e204fd6f1694633
Andrew <paintstone@gmail.com>
2022-09-12 07:26:25 -0700

    Initial commit

---
+++ CHANGEFILE.rst
@@ -0,0 +1,3 @@
+Fixing Data
+1. when pythonspeed wrote the tutorial they didnt include the data
+2. could not find via google search, found possible analog in github
---
+++ chunk_that_csv.py
@@ -0,0 +1,35 @@
+# SOURCE https://pythonspeed.com/articles/indexing-pandas-sqlite/
+# FILENAME Chunking_Down
+
+import safety
+import bandit
+import pandas as pd
+import numpy
+
+voters_street1 = pd.read
+
+def main():
+	print(pd)
+	print(numpy)
+	get_whole_file()
+
+
+def get_whole_file():
+	voters_street = pd.read.csv(
+		"voters.csv")["County"]
+	print(voters_street.value_counts())
+
+
+# function for defined dataset to scan into an index
+def get_voters_on_street(name):
+	return pd.concat(
+		df[df["county"] == name] for df in
+		pd.read.csv("voters.csv", chunksize = 1000))
+
+
+if __name__ == '__main__':
+	main()
+	print(pd)
+    #function_a()
+    #function_b()
+#print("after __name__ guard")
